0a4a9a964a862b6b4a05d281625b2482851472ab
Kicshikxo <supermegavalera9000@gmail.com>
2025-02-18 19:22:02 +0300

    feat: remove unused audio

--- src/assets/audio/ui.rs
+++ src/assets/audio/ui.rs
@@ -6,8 +6,6 @@ use bevy_asset_loader::asset_collection::AssetCollection;
 pub struct UiAudioAssets {
     #[asset(path = "embedded://audio/ui/button_click.ogg")]
     pub button_click: Handle<AudioSource>,
-    #[asset(path = "embedded://audio/ui/button_back_click.ogg")]
-    pub button_back_click: Handle<AudioSource>,
     #[asset(path = "embedded://audio/ui/level_select.ogg")]
     pub level_select: Handle<AudioSource>,
 }
