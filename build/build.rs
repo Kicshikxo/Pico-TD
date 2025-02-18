621d2f573675524a495711882907f388fa1323ab
Kicshikxo <supermegavalera9000@gmail.com>
2025-02-18 22:38:18 +0300

    feat: add windows icon

---
+++ build/build.rs
@@ -0,0 +1,8 @@
+extern crate embed_resource;
+
+fn main() {
+    #[cfg(target_os = "windows")]
+    embed_resource::compile("build/windows/icon.rc", embed_resource::NONE)
+        .manifest_optional()
+        .unwrap();
+}
