7c1c128ca753123bca7932a4ac6f109aeb6d491a
Kicshikxo <supermegavalera9000@gmail.com>
2025-02-19 10:48:20 +0300

    feat: fix build for wasm

--- build/build.rs
+++ build/build.rs
@@ -1,8 +1,9 @@
 extern crate embed_resource;
 
 fn main() {
-    #[cfg(target_os = "windows")]
-    embed_resource::compile("build/windows/icon.rc", embed_resource::NONE)
-        .manifest_optional()
-        .unwrap();
+    if std::env::var("TARGET").unwrap().contains("windows") {
+        embed_resource::compile("build/windows/icon.rc", embed_resource::NONE)
+            .manifest_optional()
+            .unwrap();
+    }
 }
