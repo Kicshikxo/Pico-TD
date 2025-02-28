1575852aaccdd8ac0a3c797ed1cdf1f40e1716c5
Kicshikxo <supermegavalera9000@gmail.com>
2025-02-28 20:46:52 +0300

    feat: improve game over screen

--- src/game/ui/views/game_over.rs
+++ src/game/ui/views/game_over.rs
@@ -32,6 +32,7 @@ struct RootUiComponent;
 
 #[derive(Component, PartialEq)]
 enum ButtonAction {
+    RestartLevel,
     BackToMenu,
 }
 
@@ -46,69 +47,68 @@ fn init_ui(mut commands: Commands, ui_assets: Res<UiAssets>, player: Res<Player>
             parent
                 .spawn(
                     UiContainer::new()
-                        .with_variant(UiContainerVariant::Primary)
+                        .with_variant(if player.get_health().is_alive() {
+                            UiContainerVariant::Success
+                        } else {
+                            UiContainerVariant::Danger
+                        })
                         .with_width(Val::Px(320.0))
-                        .with_padding(UiRect::all(Val::Px(24.0)))
+                        .with_padding(UiRect::all(Val::Px(24.0)).with_top(Val::Px(40.0)))
                         .with_row_gap(Val::Px(12.0))
                         .center()
                         .column(),
                 )
                 .with_children(|parent| {
                     parent
-                        .spawn(
+                        .spawn((
                             UiContainer::new()
-                                .with_variant(UiContainerVariant::Secondary)
-                                .with_padding(UiRect::all(Val::Px(8.0))),
-                        )
-                        .with_child(UiText::new("ui.game_over.title").with_size(UiTextSize::Large));
+                                .with_top(Val::Px(-12.0))
+                                .absolute()
+                                .center(),
+                            ZIndex(1),
+                        ))
+                        .with_children(|parent| {
+                            for star_index in 1..=3 {
+                                parent.spawn((
+                                    UiContainer::new()
+                                        .with_bottom(if star_index == 2 {
+                                            Val::Px(12.0)
+                                        } else {
+                                            Val::Px(0.0)
+                                        })
+                                        .with_width(Val::Px(48.0))
+                                        .with_height(Val::Px(48.0)),
+                                    ImageNode {
+                                        color: if star_index
+                                            <= LevelCompletionStars::from_player_health(
+                                                player.get_health(),
+                                            )
+                                            .as_index()
+                                        {
+                                            Color::srgb(1.0, 1.0, 0.0)
+                                        } else {
+                                            Color::WHITE
+                                        },
+                                        image: ui_assets.ui_misc.clone(),
+                                        texture_atlas: Some(TextureAtlas {
+                                            layout: ui_assets.ui_misc_layout.clone(),
+                                            index: UiMiscSpriteVariant::Star as usize,
+                                        }),
+                                        ..default()
+                                    },
+                                ));
+                            }
+                        });
 
                     parent
                         .spawn(
                             UiContainer::new()
-                                .with_variant(if player.get_health().is_alive() {
-                                    UiContainerVariant::Success
-                                } else {
-                                    UiContainerVariant::Danger
-                                })
-                                .with_padding(UiRect::all(Val::Px(12.0)).with_top(Val::Px(24.0)))
+                                .with_variant(UiContainerVariant::Secondary)
+                                .with_padding(UiRect::all(Val::Px(12.0)))
                                 .column()
                                 .center(),
                         )
                         .with_children(|parent| {
-                            parent
-                                .spawn(UiContainer::new().center())
-                                .with_children(|parent| {
-                                    for star_index in 1..=3 {
-                                        parent.spawn((
-                                            UiContainer::new()
-                                                .with_bottom(if star_index == 2 {
-                                                    Val::Px(8.0)
-                                                } else {
-                                                    Val::Px(0.0)
-                                                })
-                                                .with_width(Val::Px(32.0))
-                                                .with_height(Val::Px(32.0)),
-                                            ImageNode {
-                                                color: if star_index
-                                                    <= LevelCompletionStars::from_player_health(
-                                                        player.get_health(),
-                                                    )
-                                                    .as_index()
-                                                {
-                                                    Color::srgb(1.0, 1.0, 0.0)
-                                                } else {
-                                                    Color::WHITE
-                                                },
-                                                image: ui_assets.ui_misc.clone(),
-                                                texture_atlas: Some(TextureAtlas {
-                                                    layout: ui_assets.ui_misc_layout.clone(),
-                                                    index: UiMiscSpriteVariant::Star as usize,
-                                                }),
-                                                ..default()
-                                            },
-                                        ));
-                                    }
-                                });
                             parent.spawn(
                                 UiText::new(if player.get_health().is_alive() {
                                     "ui.game_over.player_win"
@@ -118,6 +118,13 @@ fn init_ui(mut commands: Commands, ui_assets: Res<UiAssets>, player: Res<Player>
                                 .with_size(UiTextSize::Large),
                             );
                         });
+
+                    parent
+                        .spawn((
+                            ButtonAction::RestartLevel,
+                            UiButton::new().with_variant(UiButtonVariant::Success),
+                        ))
+                        .with_child(UiText::new("ui.game_over.restart_level"));
 
                     parent
                         .spawn((
@@ -143,6 +150,9 @@ fn update_ui(
     for (interaction, button_action) in &interaction_query {
         if *interaction == Interaction::Pressed {
             match button_action {
+                ButtonAction::RestartLevel => {
+                    next_game_state.set(GameState::Start);
+                }
                 ButtonAction::BackToMenu => {
                     next_ui_state.set(UiState::Menu);
                     next_game_state.set(GameState::Pause);
